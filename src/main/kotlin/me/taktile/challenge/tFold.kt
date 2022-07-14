732e1d18b20539a1946b096f957d537ebfd01e0f
marten.kremser <marten.kremser@student.hpi.uni-potsdam.de>
2022-07-14 09:29:45 +0200

    add method documentation

--- src/main/kotlin/me/taktile/challenge/tFold.kt
+++ src/main/kotlin/me/taktile/challenge/tFold.kt
@@ -1,5 +1,9 @@
 package me.taktile.challenge
 
+/**
+ * tFold implements fold, a higher order function that given an initial value x of type B and a function f:(A,B) -> B
+ * consecutively applies x = f(a,x) for all a: A in this List, then returns x. It is applied from the first to the last element.
+ */
 fun <A, B> List<A>.tFold(initial: B, function: ((A, B) -> B)): B {
     var result = initial
     this.forEach {
